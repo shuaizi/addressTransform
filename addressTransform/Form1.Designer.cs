24bb264d1cf1477586983ed7a8569d0bf231036f
shuaizi <shuaizi614@gmail.com>
2014-07-16 22:32:39 +0800

    first project file add

---
+++ addressTransform/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace addressTransform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBAddr = new System.Windows.Forms.TextBox();
+            this.btnTrans = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtbAnswer = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入你要转换的地址";
+            // 
+            // txtBAddr
+            // 
+            this.txtBAddr.Location = new System.Drawing.Point(15, 27);
+            this.txtBAddr.Name = "txtBAddr";
+            this.txtBAddr.Size = new System.Drawing.Size(412, 21);
+            this.txtBAddr.TabIndex = 1;
+            // 
+            // btnTrans
+            // 
+            this.btnTrans.Location = new System.Drawing.Point(434, 25);
+            this.btnTrans.Name = "btnTrans";
+            this.btnTrans.Size = new System.Drawing.Size(59, 23);
+            this.btnTrans.TabIndex = 2;
+            this.btnTrans.Text = "转换";
+            this.btnTrans.UseVisualStyleBackColor = true;
+            this.btnTrans.Click += new System.EventHandler(this.btnTrans_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "转换结果";
+            // 
+            // rtbAnswer
+            // 
+            this.rtbAnswer.Location = new System.Drawing.Point(15, 90);
+            this.rtbAnswer.Name = "rtbAnswer";
+            this.rtbAnswer.Size = new System.Drawing.Size(478, 168);
+            this.rtbAnswer.TabIndex = 4;
+            this.rtbAnswer.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(256, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(233, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "made by zs  email:shuaizi614@gmail.com";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(339, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ctrl+C 复制  Ctrl+V 粘帖";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 282);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rtbAnswer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnTrans);
+            this.Controls.Add(this.txtBAddr);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "常用下载软件地址转换器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBAddr;
+        private System.Windows.Forms.Button btnTrans;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtbAnswer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ addressTransform/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace addressTransform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTrans_Click(object sender, EventArgs e)
+        {
+            string realUrl = "";
+            string thunderUrl = "";
+            string tencentUrl = "";
+            string flashgetUrl = "";
+            string baseURL = this.txtBAddr.Text;
+            if (baseURL.Trim().Equals(""))
+            {
+                MessageBox.Show("地址栏不能为空");
+                this.txtBAddr.Text = "";
+                this.txtBAddr.Focus();
+            }
+            else 
+            {
+                char[] sep = { '/', '/' };
+                string[] urlDeal = this.txtBAddr.Text.Trim().Split(new[] { "//" }, StringSplitOptions.None);
+                string head = urlDeal[0].Trim().ToLower();
+                String body = urlDeal[1].Trim();
+                MessageBox.Show(head + "\n" + body);
+                if (head.Equals("") || body.Equals("")) {
+                    MessageBox.Show("url格式错误");
+                    this.txtBAddr.Text = "";
+                    this.txtBAddr.Focus();
+                    return;
+                }       
+                //得到真实地址
+                if (head.Equals("thunder:")) {
+                    string tmp = this.base64Decode(body);
+                    realUrl = tmp.Substring(2, tmp.Length - 4);
+                }
+                else if (head.Equals("flashget:")) {
+                    string[] dealUrl1 = body.Split('&');
+                    string tmp = this.base64Decode(dealUrl1[0]);
+                    realUrl = tmp.Substring(10, tmp.Length - 20);
+                }
+                else if(head.Equals("qqdl:")){
+                    realUrl = this.base64Decode(body);
+                }
+                else if (head.Equals("http:") || head.Equals("https:") || head.Equals("ftp:")
+                        || head.Equals("mms:") || head.Equals("rtsp:"))
+                {
+                    realUrl = this.txtBAddr.Text.Trim();
+                }
+                else {
+                    MessageBox.Show("暂不支持此协议\n请联系作者");
+                    this.txtBAddr.Focus();
+                    return;
+                }
+                //转换各下载软件地址
+                thunderUrl = "thunder://" + this.base64Encode("AA" + realUrl + "ZZ");
+                tencentUrl = "qqdl://" + this.base64Encode(realUrl);
+                flashgetUrl = "Flashget://" + this.base64Encode("[FLASHGET]" + realUrl + "[FLASHGET]") + "&aiyh";
+                //输出结果
+                this.rtbAnswer.Text = "";
+                string lblText = "努力转换中";
+                for (int i = 0; i < 6; i++) {
+                    lblText = lblText + ".";
+                    this.label2.Text = lblText;
+                    this.label2.Update();
+                    Thread.Sleep(300);
+                }
+                this.label2.Text = "转换结果";
+                this.rtbAnswer.Text = "";
+                this.rtbAnswer.AppendText("\n原始下载地址：" + realUrl);
+                this.rtbAnswer.AppendText("\n迅雷下载地址：" + thunderUrl);
+                this.rtbAnswer.AppendText("\n快车下载地址：" + flashgetUrl);
+                this.rtbAnswer.AppendText("\n旋风下载地址：" + tencentUrl);
+            }
+
+        }
+        //base64编码
+        private string base64Encode(string baseUrl)
+        {
+            byte[] output = Encoding.Default.GetBytes(baseUrl);
+            string str = Convert.ToBase64String(output);
+            return str;
+        }
+        //base64解码
+        private string base64Decode(string baseUrl) {
+            MessageBox.Show(baseUrl);
+            byte[] output = Convert.FromBase64String(baseUrl);
+            string str = Encoding.Default.GetString(output);
+            return str;
+        }
+    }
+}
